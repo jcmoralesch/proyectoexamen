3ea651e0c399f555c86ace6f505bb14c61761aea
jcmorales <jcmoralesch@hotmail.com>
2020-08-21 12:31:31 -0600

    Creando proyecto

---
+++ spring-app-proyecto-examen/src/main/java/com/app/proyecto/examen/SpringAppProyectoExamenApplication.java
@@ -0,0 +1,13 @@
+package com.app.proyecto.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppProyectoExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppProyectoExamenApplication.class, args);
+	}
+
+}
---
+++ spring-app-proyecto-examen/src/test/java/com/app/proyecto/examen/SpringAppProyectoExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.proyecto.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppProyectoExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
