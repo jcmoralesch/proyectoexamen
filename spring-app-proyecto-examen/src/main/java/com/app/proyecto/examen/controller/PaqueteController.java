44688ac0eb6467a544d9b53db1f6734319ef4c68
jcmorales <jcmoralesch@hotmail.com>
2020-08-21 14:23:00 -0600

    Tercer commit enviado

--- spring-app-proyecto-examen/src/main/java/com/app/proyecto/examen/controller/PaqueteController.java
+++ spring-app-proyecto-examen/src/main/java/com/app/proyecto/examen/controller/PaqueteController.java
@@ -5,8 +5,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.app.proyecto.examen.model.entity.Paquete;
 import com.app.proyecto.examen.model.service.IPaqueteService;
 
@@ -39,6 +43,35 @@ public class PaqueteController {
 		return "/paquete/form";
 	}
 	
+	@PostMapping("/guardar")
+	public String guardar(Paquete paquete, RedirectAttributes flash,SessionStatus status) {
+		
+		paquete.getCantidadBolson().toUpperCase();
+		paquete.getTipo().toUpperCase();
+		paquete.getVigencia().toUpperCase();
+		
+		paqueteService.save(paquete);
+		status.setComplete();
+		flash.addFlashAttribute("success", "Operacion realizada con exito");
+		
+		return "redirect:/paquete/listar";		
+	}
 	
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable Long id,Map<String, Object> model,RedirectAttributes flash) {
+        if(id>0) {	
+        	Paquete paquete=paqueteService.findById(id);
+        	
+        	if(paquete!=null) {
+        	model.put("titulo", "Acualizar datos de paquete");
+        	model.put("paquete", paquete);
+        	return "/paquete/form";
+        	}
+        }
+        	
+        	flash.addFlashAttribute("danger", "Registro no existe en la BBDD");
+        	return "redirect:/paquete/listar";
+        	
+	}
 
 }
